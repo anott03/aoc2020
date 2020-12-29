f3e6da99a25f6ed1dc5a5eecb4d1a9b1610d0dc2
anott03 <amitav_nott@ryecountryday.org>
2020-12-29 17:13:14 -0500

    day17

--- day17/index.ts
+++ day17/index.ts
@@ -1,6 +1,31 @@
 import * as fs from 'fs';
 
-let input = fs.readFileSync('./input.test', 'utf-8').trim().split('\n')
-for (let line of input) {
+class Cube {
+  x: number;
+  y: number;
+  z: number;
+  active: boolean;
 
+  constructor(x: number, y: number, z: number, active: boolean = false) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.active = active;
+  }
+
+}
+
+let input = fs.readFileSync('./input.test', 'utf-8').trim().split('\n')
+let cubes: Cube[] = [];
+
+for (let i = 0; i < input.length; ++i) {
+  for (let j = 0; j < input[i].length; ++j) {
+    let c: string  = input[i].charAt(j);
+    cubes.push(new Cube(j, i, 0, c == '#'));
+  }
+}
+
+console.log(fs.readFileSync('./input.test', 'utf-8').trim());
+for (let cube of cubes) {
+  console.log(cube.x, cube.y, cube.z, cube.active);
 }
