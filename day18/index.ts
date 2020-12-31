a820c3edaaff905f0671ee2b7cd4726e4828207b
Amitav Nott <amitav_nott@ryecountryday.org>
2020-12-31 16:55:27 -0500

    18 part 2

--- day18/index.ts
+++ day18/index.ts
@@ -36,4 +36,41 @@ function part1() {
   }
   console.log('PART 1', sum);
 }
+
+function part2() {
+  input = fs.readFileSync('./input.prod', 'utf-8').trim().split('\n');
+  const solve = (line: string): number => {
+    let tokens = line.split('/');
+
+    while (tokens.includes('(')) {
+      let close_idx = tokens.indexOf(')');
+      let open_idx = tokens.slice(0, close_idx).lastIndexOf('(');
+      tokens = [...tokens.slice(0, open_idx),
+        solve(tokens.slice(open_idx + 1, close_idx).join('/')).toString(),
+      ...tokens.slice(close_idx + 1)]
+    }
+
+    while (tokens.includes('+')) {
+      let multIdx = tokens.indexOf('+');
+      let s = parseInt(tokens[multIdx - 1]) + parseInt(tokens[multIdx + 1]);
+      tokens = [...tokens.slice(0, multIdx - 1), s.toString(), ...tokens.slice(multIdx + 2)];
+    }
+
+    let ans = 1;
+    tokens.forEach((token: string, _) => {
+      if (token !== '*') ans *= parseInt(token);
+    });
+
+    return ans;
+  }
+
+  let sum = 0;
+  for (let line of input) {
+    let x = solve(line.replace(/ /g, '').split('').join('/'));
+    sum += x;
+  }
+  console.log('PART 2', sum);
+}
+
 part1();
+part2();
