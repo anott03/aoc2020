1d14f5dcd4ffb5a14601589d3364ec12eb8e2a30
Amitav Nott <amitav_nott@ryecountryday.org>
2020-12-31 00:04:04 -0500

    completed day 17

--- day17/index.ts
+++ day17/index.ts
@@ -58,7 +58,7 @@ const part1 = () => {
 }
 
 const part2 = () => {
-  let input = fs.readFileSync('./input.test', 'utf-8').trim().split('\n')
+  let input = fs.readFileSync('./input.prod', 'utf-8').trim().split('\n')
   let activeCubes = new Set();
   input.forEach((line, i) => {
     line.split('').forEach((c, j) => {
@@ -75,43 +75,48 @@ const part2 = () => {
       let x = parseInt(s.substring(1, s.indexOf('y')));
       let y = parseInt(s.substring(s.indexOf('y') + 1, s.indexOf('z')));
       let z = parseInt(s.substring(s.indexOf('z') + 1, s.indexOf('w')));
-      let w = parseInt(s.substring(s.indexOf('z') + 1));
-      for (let i = 1; i < Math.pow(4, 4); ++i) {
-        let b3 = i.toString(4).padStart(3, '0').split('').reverse();
-        const nx = x + (b3[0] == '2' ? -1 : parseInt(b3[0]));
-        const ny = y + (b3[1] == '2' ? -1 : parseInt(b3[1]));
-        const nz = z + (b3[2] == '2' ? -1 : parseInt(b3[2]));
-        check.add(`x${nx}y${ny}z${nz}`);
-      }
+      let w = parseInt(s.substring(s.indexOf('w') + 1));
+
+      for (let dx of [-1, 0, 1]) { for (let dy of [-1, 0, 1]) { for (let dz of [-1, 0, 1]) { for (let dw of [-1, 0, 1]) {
+        const nx = x + dx;
+        const ny = y + dy;
+        const nz = z + dz;
+        const nw = w + dw;
+        check.add(`x${nx}y${ny}z${nz}w${nw}`);
+      }}}}
     });
 
     check.forEach((s: string) => {
       let activeNeighbors = 0;
       let x = parseInt(s.substring(1, s.indexOf('y')));
       let y = parseInt(s.substring(s.indexOf('y') + 1, s.indexOf('z')));
-      let z = parseInt(s.substring(s.indexOf('z') + 1));
-      for (let i = 1; i < 27; ++i) {
-        let b3 = i.toString(3).padStart(3, '0').split('').reverse();
-        const nx = x + (b3[0] == '2' ? -1 : parseInt(b3[0]));
-        const ny = y + (b3[1] == '2' ? -1 : parseInt(b3[1]));
-        const nz = z + (b3[2] == '2' ? -1 : parseInt(b3[2]));
+      let z = parseInt(s.substring(s.indexOf('z') + 1, s.indexOf('w')));
+      let w = parseInt(s.substring(s.indexOf('w') + 1));
 
-        if (activeCubes.has(`x${nx}y${ny}z${nz}`)) {
-          ++activeNeighbors;
+      for (let dx of [-1, 0, 1]) { for (let dy of [-1, 0, 1]) { for (let dz of [-1, 0, 1]) { for (let dw of [-1, 0, 1]) {
+        if (dx !== 0 || dy !== 0 || dz !== 0 || dw !== 0) {
+          const nx = x + dx;
+          const ny = y + dy;
+          const nz = z + dz;
+          const nw = w + dw;
+
+          if (activeCubes.has(`x${nx}y${ny}z${nz}w${nw}`)) {
+            ++activeNeighbors;
+          }
         }
-      }
+      }}}}
 
-      if (activeCubes.has(`x${x}y${y}z${z}`)) {
-        if (activeNeighbors === 2 || activeNeighbors === 3) newActiveCubes.add(`x${x}y${y}z${z}`);
+      if (activeCubes.has(`x${x}y${y}z${z}w${w}`)) {
+        if (activeNeighbors === 2 || activeNeighbors === 3) newActiveCubes.add(`x${x}y${y}z${z}w${w}`);
       } else {
-        if (activeNeighbors === 3) newActiveCubes.add(`x${x}y${y}z${z}`);
+        if (activeNeighbors === 3) newActiveCubes.add(`x${x}y${y}z${z}w${w}`);
       }
     });
 
     console.log(newActiveCubes);
     activeCubes = newActiveCubes;
   }
-  console.log('PART 1', activeCubes.size);
+  console.log('PART 2', activeCubes.size);
 }
 
 // part1();
